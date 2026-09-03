d6ad6dd1718ca05ad987a1ecf0e4daf5da4df595
LocoMosquito-hash <juanpis@gmail.com>
2026-09-02 19:31:09 -0500

    Code Coverage and Quality

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -1,13 +1,13 @@
 # project/app/api/summaries.py
 
 
+from typing import List
+
 from fastapi import APIRouter, HTTPException
 
 from app.api import crud
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
 from app.models.tortoise import SummarySchema
-from typing import List
-
 
 router = APIRouter()
 
@@ -16,10 +16,7 @@ router = APIRouter()
 async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema:
     summary_id = await crud.post(payload)
 
-    response_object = {
-        "id": summary_id,
-        "url": payload.url
-    }
+    response_object = {"id": summary_id, "url": payload.url}
     return response_object
 
 
